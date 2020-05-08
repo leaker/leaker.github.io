a4c094d16e4bf2abf270e636e3ea4ce77eea1756
leaker <admin@leelib.com>
2020-05-08 20:35:25 +0800

    * 修正国内域名为 www.kaij.cn

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,10 @@ if [ -d "public" ]; then
   hexo clean
 fi
 hexo d
+
+echo 正在生成urls.txt...
+grep -o "https://www.leelib.com/[^<]*" public/sitemap.xml > urls.txt
+echo 修正域名用于阿里云CDN预热...
+sed -i s/"www.leelib.com"/"www.kaij.cn"/ urls.txt
+#echo 百度链接提交...
+#curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.kaij.cn&token=qyTc2OCtwlf52Y64"
