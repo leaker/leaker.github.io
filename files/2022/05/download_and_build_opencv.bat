c8781344dd72aceef20bb63d5c921b0b02763533
leaker <leaker@users.noreply.github.com>
2024-01-10 15:06:51 +0000

    deploy: f6ddc0ccde392fbfdbeeeabb5698ad4f1b81d390

--- files/2022/05/download_and_build_opencv.bat
+++ files/2022/05/download_and_build_opencv.bat
@@ -1,13 +1,14 @@
+
 @ECHO OFF
 ECHO -- Downloading OpenCV Source Code --
 wget https://github.com/opencv/opencv/archive/refs/tags/4.6.0.zip -O opencv-4.6.0.zip
-IF NOT ERRORLEVEL 0 GOTO :end
-7z x opencv-4.6.0.zip
-IF NOT ERRORLEVEL 0 GOTO :end
+IF %ERRORLEVEL% NEQ 0 GOTO :error
+7z x -y opencv-4.6.0.zip
+IF %ERRORLEVEL% NEQ 0 GOTO :error
 wget https://github.com/opencv/opencv_contrib/archive/refs/tags/4.6.0.zip -O opencv_contrib-4.6.0.zip
-IF NOT ERRORLEVEL 0 GOTO :end
-7z x opencv_contrib-4.6.0.zip
-IF NOT ERRORLEVEL 0 GOTO :end
+IF %ERRORLEVEL% NEQ 0 GOTO :error
+7z x -y opencv_contrib-4.6.0.zip
+IF %ERRORLEVEL% NEQ 0 GOTO :error
 SET "USERHOME=%USERPROFILE:\=/%"
 SET OPENCV_SRC_DIR=%cd%/opencv-4.6.0
 SET OPENCV_CONTRIB_SRC_DIR=%cd%/opencv_contrib-4.6.0
@@ -24,7 +25,7 @@ ECHO -- Starting OpenCV Configuration --
 call "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat"
 @REM If you want to use the Visual Studio 2019 MSVC:
 @REM call "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat" -vcvars_ver=14.29
-cmake.exe ^
+cmake ^
 -S%OPENCV_SRC_DIR% ^
 -B%OPENCV_BUILD_DIR% ^
 -DCMAKE_INSTALL_PREFIX=%OPENCV_BUILD_DIR%/install ^
@@ -58,22 +59,25 @@ cmake.exe ^
 -DBUILD_opencv_js=OFF ^
 -GNinja ^
 -DBUILD_opencv_world=ON
-IF NOT ERRORLEVEL 0 GOTO :end
-IF NOT ERRORLEVEL 0 GOTO :end
+IF %ERRORLEVEL% NEQ 0 GOTO :error
 ECHO -- OpenCV Configuration has finished, press any key to proceeding to build phase... --
-PAUSE
-cmake.exe --build %OPENCV_BUILD_DIR% --target install
-IF NOT ERRORLEVEL 0 GOTO :end
+PAUSE > NUL
+cmake --build %OPENCV_BUILD_DIR% --target install
+IF %ERRORLEVEL% NEQ 0 GOTO :error
 ECHO -- OpenCV Build has finished, press any key to install... --
-PAUSE
+PAUSE > NUL
 pip uninstall opencv-python opencv-contrib-python
-IF NOT ERRORLEVEL 0 GOTO :end
+IF %ERRORLEVEL% NEQ 0 GOTO :error
 COPY %OPENCV_BUILD_DIR%/lib/python3/cv2.cp310-win_amd64.pyd %PYTHON_PATH%/Lib/site-packages/cv2.cp310-win_amd64.pyd /Y
-IF NOT ERRORLEVEL 0 GOTO :end
+IF %ERRORLEVEL% NEQ 0 GOTO :error
 COPY %OPENCV_BUILD_DIR%/install/x64/vc17/bin/*.dll %PYTHON_PATH%/Scripts /Y
-IF NOT ERRORLEVEL 0 GOTO :end
+IF %ERRORLEVEL% NEQ 0 GOTO :error
 pip install opencv-contrib-python
-IF NOT ERRORLEVEL 0 GOTO :end
+IF %ERRORLEVEL% NEQ 0 GOTO :error
 ECHO -- all finished, press any key to exit... --
-PAUSE
-:end
+PAUSE > NUL
+GOTO :EOF
+
+:error
+ECHO -- Error has occurred!!! press any key to exit... --
+PAUSE > NUL
